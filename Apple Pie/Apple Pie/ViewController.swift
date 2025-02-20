5b676284d5b8ba538470b5f02d4dd49b60b11cf5
David Santiago Jamaica Galvis <davidjamaica@Davids-MacBook-Air-2.local>
2025-02-20 10:00:19 -0700

    Calculator update

---
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by David Santiago Jamaica Galvis on 2/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Calculadora/Calculadora/ViewController.swift
+++ Calculadora/Calculadora/ViewController.swift
@@ -4,16 +4,81 @@
 //
 //  Created by David Santiago Jamaica Galvis on 1/31/25.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    // Create a String variable that holds what is supposed to be displayed on the displayLabel
+    var currentValue: String = ""
+    // Store the current displayed value in a second variable called "previousValue"
+    var previousValue: String = ""
+    // When an operator (+, -, /, *) is pressed:
+    var currentOperator: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        if let buttonPressed = sender.titleLabel?.text {
+            currentValue += buttonPressed
+            displayLabel.text = currentValue
+        }
+    }
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        if let operatorPressed = sender.titleLabel?.text {
+            previousValue = currentValue
+            currentOperator = operatorPressed
+            currentValue = ""
+            displayLabel.text = "0"
+        }
+    }
+    
+    @IBAction func equalsButtonPressed(_ sender: UIButton) {
+        if let previous = Double(previousValue), let current = Double(currentValue) {
+            var result: Double = 0
+            switch currentOperator {
+            case "+":
+                result = previous + current
+            case "-":
+                result = previous - current
+            case "*":
+                result = previous * current
+            case "/":
+                if current != 0 {
+                    result = previous / current
+                } else {
+                    displayLabel.text = "Error"
+                    return
+                }
+            default:
+                break
+            }
+            
+            displayLabel.text = "\(result)"
+            currentValue = "\(result)"
+            previousValue = ""
+            currentOperator = ""
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        currentValue = ""
+        previousValue = ""
+        currentOperator = ""
+        displayLabel.text = "0"
+    }
+    @IBAction func decimalButtonPressed(_ sender: UIButton) {
+        if !currentValue.contains(".") {
+            currentValue += "."
+            displayLabel.text = currentValue
+        }
+        
+        
+        
+    }
 }
+
+
 
