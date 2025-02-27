6d228e3fbfa21f399e9eb5c00cae10ca4e4e2096
David Santiago Jamaica Galvis <davidjamaica@Davids-MacBook-Air-2.local>
2025-02-26 19:50:07 -0700

    Tally app finish

---
+++ Tally App/Tally AppTests/Tally_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_AppTests.swift
+//  Tally AppTests
+//
+//  Created by David Santiago Jamaica Galvis on 2/26/25.
+//
+
+import Testing
+@testable import Tally_App
+
+struct Tally_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
