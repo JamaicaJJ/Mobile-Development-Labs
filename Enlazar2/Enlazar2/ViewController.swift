32ddf1c4d9a48f0cbac42e5f623d9c0b31f4c2b8
David Santiago Jamaica Galvis <davidjamaica@Davids-MacBook-Air-2.local>
2025-02-28 09:49:39 -0700

    Enlazar part 1 finish

--- Enlazar2/Enlazar2/ViewController.swift
+++ Enlazar2/Enlazar2/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageViewWidthConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.layer.cornerRadius = imageViewWidthConstraint.constant / 2
     }
-
-
 }
 
