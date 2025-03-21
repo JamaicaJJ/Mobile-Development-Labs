c05dbb4129869489372d2b6da8847b7b49d614d8
David Santiago Jamaica Galvis <davidjamaica@Davids-MacBook-Air-2.local>
2025-03-21 09:41:57 -0600

    FamilyProyectV2 finish

---
+++ FamilyProyectV2/FamilyProyectV2/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  FamilyProyectV2
+//
+//  Created by David Santiago Jamaica Galvis on 3/20/25.
+//
+
+import Foundation
---
+++ FamilyProyectV2/FamilyProyectV2Tests/FamilyProyectV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyProyectV2Tests.swift
+//  FamilyProyectV2Tests
+//
+//  Created by David Santiago Jamaica Galvis on 3/20/25.
+//
+
+import Testing
+@testable import FamilyProyectV2
+
+struct FamilyProyectV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
